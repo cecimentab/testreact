6b7c7c4649e9d6d3046db4ad1918e6b9dca3e2c6
cecilia-mendozatabares <cecilia-mendozatabares>
2024-02-01 16:48:38 +0000

    useRef

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
+import { useRef } from 'react';
 
 function App() {
-  const [checked, setCheck] = useReducer((checked) => !checked, false);
+  // const [checked, s etCheck] = useReducer((checked) => !checked, false);
+  const txtTitle = useRef();
+  const hexColor = useRef();
 
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    console.log(`${title}, ${color}`)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <input type="checkbox" value={checked} onChange={setCheck} />
-        <label>{checked ? "checked" : "not checked"}</label>
-      </header>
-      
-    </div>
+    // **useReducer**
+    // <div className="App">
+    //   <header className="App-header">
+    //     <input type="checkbox" value={checked} onChange={setCheck} />
+    //     <label>{checked ? "checked" : "not checked"}</label>
+    //   </header>
+    // </div>
+
+    //**useRef */
+    <form onSubmit={submit}>
+      <input
+        ref={txtTitle}
+        type = "text"
+        placeholder="color title.."
+      />
+      <input ref={hexColor} type="color" />
+      <button>ADD</button>
+    </form>
   );
 }
 
